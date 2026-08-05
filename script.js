bb5d8bff80f57dbe9f1e601a0b8fe2e5f59ce59f
JaviG <86110696+s9code@users.noreply.github.com>
2026-08-05 11:19:16 +0200

    Lógica de entrada implementada para el primer y el segundo operando.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// SELECTORES
+
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector("#display");
+const operadorBtn = document.querySelectorAll(".operator");
+
+// VARIABLES --------------
 
 let numero;
 
 let operador;
 
 let otroNumero;
+
+// FUNCIONES --------------
 
 function sumar(num1, num2) {
     return num1 + num2;
@@ -32,9 +41,67 @@ function operate(operador, num1, num2) {
         return dividir(num1, num2);
     }
         
-}
+};
 
-console.log(operate("+", 4, 9));
-console.log(operate("-", 4, 2));
-console.log(operate("*", 4, 2));
-console.log(operate("/", 4, 2));
+// EVENTOS --------------
+
+
+// BOTONES NUMÉRICOS
+
+// Mostrar números en pantalla
+
+// Construir primer número
+
+// Construir segundo número
+
+
+numbers.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (operador === undefined) {
+            if (display.textContent === "0") {
+            display.textContent = button.textContent;
+            numero = display.textContent;
+            } else {
+                display.textContent = display.textContent + button.textContent;
+                numero = display.textContent;
+            }
+        } else {
+            if (otroNumero === undefined) {
+                display.textContent = button.textContent;
+                otroNumero = display.textContent;
+            } else {
+                display.textContent = display.textContent + button.textContent;
+                otroNumero = display.textContent;
+            }
+        }
+        
+    });
+});
+
+
+// BOTONES OPERADORES
+
+// Guardar primer número
+
+// Guardar operador
+
+
+operadorBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        numero = display.textContent;
+        operador = button.textContent;
+
+        console.log(numero);
+        console.log(operador);
+    });
+});
+
+
+// BOTÓN =
+
+// Llamar a operate()
+
+// Mostrar resultado
+
+
+
