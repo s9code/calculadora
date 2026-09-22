9d26ce31d24837bb0bff236a5201890a825fd2a7
JaviG <86110696+s9code@users.noreply.github.com>
2026-09-22 13:09:00 +0200

    feat: Limpieza de código, eliminación de console.log y comentarios redundantes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // SELECTORES
-
 let numeros = document.querySelectorAll("#numero");
 let display = document.querySelector("#display");
 let operaciones = document.querySelectorAll("#operador");
@@ -7,11 +6,8 @@ let igual = document.querySelector("#igual");
 let clear = document.querySelector("#clear");
 
 // VARIABLES
-
 let numero;
-
 let operador;
-
 let otroNumero;
 
 // FUNCIONES
@@ -33,7 +29,7 @@ function dividir(num1, num2) {
 };
 
 function operacion (operador, num1, num2) {
-    if (operador === "+") {
+    if (operador === "+") { 
         return (sumar(num1, num2));
     } else if (operador === "-") {
         return (restar(num1, num2));
@@ -46,94 +42,51 @@ function operacion (operador, num1, num2) {
 
 // EVENTOS
 
- numeros.forEach((boton) => {
-
+numeros.forEach((boton) => {
     boton.addEventListener("click", () => {
-
-        //display.textContent = boton.textContent;
-
         if (display.textContent === "0") {
             display.textContent = boton.textContent;
         } else {
             display.textContent = display.textContent + boton.textContent;
         };
 
-        
         if (operador === undefined) {
             numero = display.textContent;
             numero = Number(numero);
-
-            console.log(numero);
-            //console.log("numero")
-
         } else {
             otroNumero = display.textContent;
             otroNumero = Number(otroNumero);
-
-            console.log(otroNumero);
-            //console.log("otroNumero");
         };
-
     });
-
 });
 
-
 operaciones.forEach((boton) => {
-
     boton.addEventListener("click", () => {
-
         display.textContent = "";
         
         if (otroNumero !== undefined) {
             numero = operacion(operador, numero, otroNumero);
             display.textContent = numero;
             otroNumero = undefined;
-             display.textContent = "";
+            display.textContent = "";
         }
-        
+
         operador = boton.textContent;
-        console.log(operador); 
-
     });
-
 });
 
 
 igual.addEventListener("click", () => {
-    console.log(igual.textContent);
-
-     display.textContent = operacion(operador, numero, otroNumero);
-
-     let recorteDecimales = Math.trunc(display.textContent * 100) / 100;
-
-     display.textContent = recorteDecimales;
-     
-     //console.log(recorteDecimales);
-     
-     
+    display.textContent = operacion(operador, numero, otroNumero);
+    
+    // Recorta los decimales
+    let recorteDecimales = Math.trunc(display.textContent * 100) / 100;
+    display.textContent = recorteDecimales;
 });
 
 clear.addEventListener("click", () => {
-    console.log(clear.textContent);
-
     display.textContent = "0";
     operador = undefined;
     numero = undefined;
     otroNumero = undefined;
 });
-
-
-// PRUEBAS 
-
-//console.log(sumar(2, 3));
-//console.log(restar(2,2));
-//console.log(multiplicar(3, 3));
-//console.log(dividir(6, 2));
-//console.log(operacion(operador, 3, 3));
-
-
-
-
-
-
